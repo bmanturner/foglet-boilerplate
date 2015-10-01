a15cb355d6193c740716eea67c4a6522e05bae16
Brendan Turner <brendanfturner@gmail.com>
2015-09-30 19:28:09 -0500

    leftNav testing

--- both/components/FogletHeader.jsx
+++ both/components/FogletHeader.jsx
@@ -1,16 +1,36 @@
-var {RaisedButton} = MUI;
+var {RaisedButton,LeftNav,MenuItem} = MUI;
 
 FogletHeader = React.createClass({
   render() {
-    return  (
-      <div>
+    var menuItems = [
+      { route: 'get-started', text: 'Get Started' },
+      { route: 'customization', text: 'Customization' },
+      { route: 'components', text: 'Components' },
+      { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
+      {
+         type: MenuItem.Types.LINK,
+         payload: 'https://github.com/callemall/material-ui',
+         text: 'GitHub'
+      },
+      {
+         text: 'Disabled',
+         disabled: true
+      },
+      {
+         type: MenuItem.Types.LINK,
+         payload: 'https://www.google.com',
+         text: 'Disabled Link',
+         disabled: true
+      },
+    ];
+    return  <div>
+        <LeftNav ref="leftNav" docked={false} menuItems={menuItems} />
         <RaisedButton
           label="Open Nav"
           onClick={this._openLeftNav} />
       </div>
-    );
   },
-  _openLeftNav: (event) => {
-    console.log('thing');
-  }
+  _openLeftNav() {
+    this.refs.leftNav.toggle();
+  },
 });
