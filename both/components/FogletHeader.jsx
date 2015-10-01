f82779f13f08cf49945f1c2186c383eca9c2888b
Brendan Turner <brendanfturner@gmail.com>
2015-10-01 11:37:55 -0500

    added materialize; no more SSR problem

--- both/components/FogletHeader.jsx
+++ both/components/FogletHeader.jsx
@@ -1,8 +1,8 @@
 FogletHeader = React.createClass({
   render() {
     return  (
-      <div>
-      </div>
+      <nav className="light-blue lighten-1" role="navigation">
+      </nav>
     );
   }
 });
